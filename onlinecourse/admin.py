815a79a1a0d921e7fbf4ca4fbaafe6a5e73efdda
thakreraj208 <thakre.raj208@gmail.com>
2026-09-11 22:35:05 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,52 @@
+# from django.contrib import admin
+# # <HINT> Import any new Models here
+# from .models import Course, Lesson, Instructor, Learner
+
+# # <HINT> Register QuestionInline and ChoiceInline classes here
+
+
+# class LessonInline(admin.StackedInline):
+#     model = Lesson
+#     extra = 5
+
+
+# # Register your models here.
+# class CourseAdmin(admin.ModelAdmin):
+#     inlines = [LessonInline]
+#     list_display = ('name', 'pub_date')
+#     list_filter = ['pub_date']
+#     search_fields = ['name', 'description']
+
+
+# class LessonAdmin(admin.ModelAdmin):
+#     list_display = ['title']
+
+
+# # <HINT> Register Question and Choice models here
+
+# admin.site.register(Course, CourseAdmin)
+# admin.site.register(Lesson, LessonAdmin)
+# admin.site.register(Instructor)
+# admin.site.register(Learner)
+
+
 from django.contrib import admin
+
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 
 class LessonInline(admin.StackedInline):
@@ -11,6 +55,7 @@ class LessonInline(admin.StackedInline):
 
 
 # Register your models here.
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
@@ -19,7 +64,13 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
     list_display = ['title']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question']
 
 
 # <HINT> Register Question and Choice models here
@@ -28,3 +79,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
